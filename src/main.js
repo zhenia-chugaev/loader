91939521f18265605b85b068484121e12591f2dd
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-10-30 13:45:02 +0100

    add page loading

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-const loadPage = () => {
-  console.log('Hello, world!');
+import fs from 'fs/promises';
+import path from 'path';
+import axios from 'axios';
+
+const regexp = /[^a-zA-Z0-9]/g;
+const sep = '-';
+
+const getFilename = (url) => {
+  const { hostname, pathname, search } = new URL(url);
+  const basename = [hostname, pathname, search]
+    .join('')
+    .replaceAll(regexp, sep);
+  return `${basename}.html`;
+};
+
+const loadPage = (url, outDir = '.') => {
+  const filename = getFilename(url);
+  const filepath = path.resolve(process.cwd(), outDir, filename);
+
+  return axios.get(url)
+    .then(({ data }) => fs.writeFile(filepath, data))
+    .then(() => ({ filepath }));
 };
 
 export default loadPage;
