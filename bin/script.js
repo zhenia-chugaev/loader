570597a63eb48e3f94c608d2ecdd6efee69a14b5
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-10-30 11:55:59 +0100

    add cli utility definition

--- bin/script.js
+++ bin/script.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
+import { Command } from 'commander';
 import loadPage from '../src/main.js';
 
-loadPage();
+const program = new Command();
+
+program
+  .name('page-loader')
+  .description('Page loader utility')
+  .version('0.1.0', '-v, --version')
+  .option('-o, --output [dir]', 'output dir', process.cwd())
+  .argument('<url>')
+  .allowExcessArguments(false);
+
+program.action((url, { output }) => {
+  loadPage(url, output).then(({ filepath }) => console.log(filepath));
+});
+
+program.parse(process.argv);
