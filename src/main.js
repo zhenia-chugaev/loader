a6a291ad6a28b56219008f92942bf4d0461591f1
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-11-03 14:55:09 +0100

    add images loading

--- src/main.js
+++ src/main.js
@@ -1,28 +1,78 @@
 import fs from 'fs/promises';
 import path from 'path';
 import axios from 'axios';
+import { load } from 'cheerio';
 
 const regexp = /[^a-zA-Z0-9]/g;
 const sep = '-';
 
-const getFilename = (url) => {
+const getName = (url, suffix = '') => {
   const { hostname, pathname, search } = new URL(url);
-  const basename = [hostname, pathname, search]
-    .join('')
-    .replaceAll(regexp, sep);
-  return `${basename}.html`;
+  const name = [
+    hostname,
+    pathname === '/' ? '' : pathname,
+    search,
+  ].join('').replaceAll(regexp, sep);
+  return `${name}${suffix}`;
 };
 
-const loadPage = (url, outDir = '.') => {
-  let filepath;
+const getAssetName = (url) => {
+  const { hostname, pathname } = new URL(url);
+  const basename = path.basename(pathname);
+  const dirname = path.dirname(pathname);
+  const name = [hostname, dirname].join('').replaceAll(regexp, sep);
+  return `${name}${sep}${basename}`;
+};
 
-  const result = Promise.resolve()
-    .then(() => getFilename(url))
-    .then((filename) => {
+const loadImage = (imageUrl, outDir) => {
+  const options = {
+    responseType: 'arraybuffer',
+  };
+
+  const result = axios.get(imageUrl, options)
+    .then(({ data }) => {
+      const imageName = getAssetName(imageUrl);
+      const imagePath = path.join(outDir, imageName);
+      return fs.writeFile(imagePath, data);
+    });
+
+  return result;
+};
+
+const loadPage = (pageUrl, outDir = '.') => {
+  let filepath;
+  let assetsPath;
+  let imagesUrls;
+
+  const result = axios.get(pageUrl)
+    .then(({ data }) => {
+      const $ = load(data, { baseURI: pageUrl });
+
+      const filename = getName(pageUrl, '.html');
       filepath = path.resolve(process.cwd(), outDir, filename);
+
+      const dirname = getName(pageUrl, '_files');
+      assetsPath = path.resolve(process.cwd(), outDir, dirname);
+
+      const $images = $('img');
+
+      imagesUrls = $images
+        .map((_, img) => $(img).prop('src'))
+        .toArray();
+
+      $images.each((_, img) => {
+        const $image = $(img);
+        const imageName = getAssetName($image.prop('src'));
+        const imagePath = path.join(dirname, imageName);
+        $image.attr('src', imagePath);
+      });
+
+      return fs.writeFile(filepath, $.html());
     })
-    .then(() => axios.get(url))
-    .then(({ data }) => fs.writeFile(filepath, data))
+    .then(() => fs.mkdir(assetsPath))
+    .then(() => Promise.all(
+      imagesUrls.map((url) => loadImage(url, assetsPath)),
+    ))
     .then(() => ({ filepath }));
 
   return result;
