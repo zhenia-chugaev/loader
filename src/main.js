f227ca49be245d929597c37f4f4e93a832c4d825
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-11-18 10:36:40 +0100

    refactor function call argument

--- src/main.js
+++ src/main.js
@@ -42,7 +42,7 @@ const loadPage = (pageUrl, outDir = '.', options = {}) => {
           const $asset = $(asset);
           const source = getSource($asset);
           const url = new URL(source, pageUrl);
-          setSource($asset, url);
+          setSource($asset, url.toString());
         })
         .filter((_, asset) => {
           const assetUrl = getSource($(asset));
